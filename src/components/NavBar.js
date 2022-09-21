fc579ef105c55f32688349e2469824fd6c5eedc5
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-09-22 01:24:17 +0300

    attempted

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,14 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const pageLinks = links.map((link) => {
+    return (
+      <a key={link} href="#link">{link}</a>
+    )
+  })
+  console.log(pageLinks)
+  return <nav>{pageLinks}</nav>;
+  
 }
 
 export default NavBar;
