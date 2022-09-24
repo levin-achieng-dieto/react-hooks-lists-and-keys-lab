897287f7fe4c5f085ddc1390413ef35bfeb0f0a4
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-09-24 14:31:41 +0300

    Completed assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ function NavBar() {
 
   const pageLinks = links.map((link) => {
     return (
-      <a key={link} href="#link">{link}</a>
+      <a key={link} href={`#${link}`}>{link}</a>
     )
   })
   console.log(pageLinks)
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,14 +2,16 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
-  const ProjectItem = projects.map((project) => {
-    return <li key={project.id}>{project}</li>
+  
+  const ProjectElements= projects.map((project) => {
+    return (
+        <ProjectItem key={project.id} name={project.name} about={project.about} technologies={project.technologies} />
+    )
   })
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list"><ProjectItem></ProjectItem></div>
+      <div id="project-list">{ProjectElements}</div>
     </div>
   );
 }
